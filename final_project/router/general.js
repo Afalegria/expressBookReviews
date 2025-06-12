9f7956ac1ab9e5eb20334709eade85a4b1306c6f
Afalegria <afalegria@uc.cl>
2025-06-12 16:37:06 -0400

    task 3 update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,10 +10,12 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   res.send(JSON.stringify(books,null,4))
 });
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
@@ -26,6 +28,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
     }
  });
   
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const requestedAuthor = req.params.author.toLowerCase();
@@ -42,7 +45,7 @@ public_users.get('/author/:author',function (req, res) {
         }
       }
     }
-  
+
     if (booksByAuthor.length > 0) {
       // Send the list of found books
       return res.json(booksByAuthor);
@@ -52,16 +55,39 @@ public_users.get('/author/:author',function (req, res) {
     }
   });
 
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const requestedTitle = req.params.title.toLowerCase();
+    let booksByTitle = [];
+
+    // Iterate directly over the keys of the books object
+    for (const key in books) {
+        // Ensure the key is a direct property of the object
+        if (books.hasOwnProperty(key)) {
+            const book = books[key];
+            // Perform a case-insensitive comparison for the title
+            if (book.title.toLowerCase() === requestedTitle) {
+                booksByTitle.push(book);
+            }
+        }
+    }
+
+    if (booksByTitle.length > 0) {
+        // Send the list of found books
+        return res.json(booksByTitle);
+    } else {
+        // If no books are found, return a 404 error
+        return res.status(404).json({ message: "No books found with this title" });
+    }
+  });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
+
 
 module.exports.general = public_users;
