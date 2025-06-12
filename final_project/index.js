4e4b75b2518e29274eae7394b1862cb8ad393e5d
Afalegria <afalegria@uc.cl>
2025-06-12 15:12:08 -0400

    task 1 update

--- final_project/index.js
+++ final_project/index.js
@@ -8,10 +8,35 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}));
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if (req.session.authorization) {
+        // Retrieve the access token from the session
+        let token = req.session.authorization['accessToken'];
+
+        // Verify the token using the secret key
+        // Note: It's best practice to store secrets in environment variables
+        jwt.verify(token, "fingerprint_customer", (err, user) => {
+            if (!err) {
+                // If the token is valid, attach the user payload to the request object
+                req.user = user;
+                // Proceed to the next middleware or route handler
+                next();
+            } else {
+                // If the token is invalid or expired, send a 403 Forbidden response
+                return res.status(403).json({
+                    message: "User not authenticated."
+                });
+            }
+        });
+    } else {
+        // If there is no token in the session, the user is not logged in
+        return res.status(403).json({
+            message: "User not logged in."
+        });
+    }
+
 });
  
 const PORT =5000;
